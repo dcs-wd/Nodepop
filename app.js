fa1aaf8da4c633ded101e491e211c3a9d91a67f2
Daniel Castillo <dcsepulveda@hotmail.es>
2018-02-15 05:02:41 +0100

    parsing ccode and add documentation

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set("view engine", "ejs");
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -28,6 +28,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", require("./routes/index"));
 app.use("/users", require("./routes/users"));
 
+// API middleware
+app.use("/apiv1/products", require("./routes/apiv1/products"));
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error("Not Found");
@@ -37,13 +40,33 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+  if (err.array) {
+    // validation error
+    err.status = 422;
+    const errInfo = err.array({ onlyFirstError: true })[0];
+    err.message = `Not valid - ${errInfo.param} ${errInfo.msg}`;
+  }
+
+  res.status(err.status || 500);
+
+  // si es una petición de API, respondemos con JSON
+  if (isAPI(req)) {
+    res.json({ success: false, error: err.message });
+    return;
+  }
+
+  // Respondo con una página de error
+
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
   res.render("error");
 });
+
+function isAPI(req) {
+  return req.originalUrl.indexOf("/apiv") === 0;
+}
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", {
+    tagTittle: "API - Nodepop",
+    title: "Nodepop",
+    description: "Practica fundamentos Nodejs con MongoDB por Daniel Castillo",
+    doc: "../doc/index.html"
+  });
 });
 
 module.exports = router;
